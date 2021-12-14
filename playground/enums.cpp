923298b3cfe1b4b0ee4e118c91845bfdf5d36323
Vineet <vineet.keshari@gmail.com>
2021-12-14 15:15:41 -0800

    More enum tests

--- playground/enums.cpp
+++ playground/enums.cpp
@@ -32,10 +32,22 @@ void test_numbered_enum() {
   int c2 = BLUE;
   std::cout << "Can assign unqualified enum to an integer." << std::endl;
 
-  Color c3 {ORANGE};
-  if (c3 == 8) {
+  Color c3 = static_cast<Color>(2); // Can assign int to an enum with cast.
+  std::cout << "Can assign cast integer to an enum." << std::endl;
+
+  Color c4 {ORANGE};
+  if (c4 == 8) {
     std::cout << "enum values are assigned sequentially." << std::endl;
   }
+}
+
+void test_enum_range() {
+  std::cout << "TEST ENUM RANGE" << std::endl;
+  enum Color {RED, BLUE, GREEN=12};
+  Color c1 = static_cast<Color>(100); // Can assign any value.
+  std::cout << "c1 is " << c1 << std::endl;
+  Color c2 = static_cast<Color>('A'); // Can assign other castable types too.
+  std::cout << "c2 is " << c2 << std::endl;
 }
 
 void test_two_enums() {
@@ -61,10 +73,28 @@ void test_enum_class() {
   // std::cout << "c1 is " << c1 << std::endl; Cannot print class enum.
 }
 
+void test_typed_enum_class() {
+  std::cout << "TEST TYPED ENUM CLASS" << std::endl;
+  enum class Color : uint32_t {RED, BLUE, GREEN};
+  // Color c1 {RED}; Must qualify with class name.
+  Color c1 {Color::RED};
+  // std::cout << "c1 is " << c1 << std::endl; Cannot print class enum.
+  Color c2 = static_cast<Color>(100); // Can assign any uint32_t.
+  Color c3 = static_cast<Color>('A'); // Can assign any other castable type too.
+
+  enum class Difficulty : char {EASY = 'A', MEDIUM = 'B', HARD = 'C'};
+  Difficulty d1 {Difficulty::EASY};
+  Difficulty d2 = static_cast<Difficulty>('D');
+  // Difficulty d2 {'E'}; Need to cast even if its the same type.
+  Difficulty d3 = static_cast<Difficulty>(5);
+}
+
 int main() {
   test_basic_enum();
   test_numbered_enum();
+  test_enum_range();
   test_two_enums();
   test_enum_class();
+  test_typed_enum_class();
   return 0;
 }
