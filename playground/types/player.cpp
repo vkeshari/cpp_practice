6ff29d5122d78e2f5fab4c24e1eb7996572b16e0
Vineet K <vkeshari@VK-Macbook.local>
2023-10-26 02:05:27 +0100

    Add Player class

---
+++ playground/types/player.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+namespace player {
+
+class Player {
+ public:
+  Player(std::string name) : name{name} { }
+
+  int& operator[](int i) { // make operators refs
+    return scores[i];
+  };
+
+  const std::string& Name() const { // make getters const, make returned ref const
+    return name;
+  }
+  const std::vector<int>& Scores() const { // make getters const, make returned ref const
+    return scores;
+  }
+  int NumScores() const { // make getters const, no need to make returned value const
+    return scores.size();
+  }
+
+  void AddScore(int score) {
+    scores.push_back(score);
+  }
+
+ private:
+  std::string name;
+  std::vector<int> scores;
+};
+
+void show_player(const Player& p) {
+  std::cout << "Player " << p.Name() << " has " << p.NumScores() << " scores:";
+  for (int s : p.Scores()) {
+    std::cout << " " << s;
+  }
+  std::cout << std::endl;
+}
+
+} // namespace player
+
+namespace player_test {
+
+void test_player() {
+  player::Player p {"Sachin"};
+  p.AddScore(200);
+  p.AddScore(186);
+  p.AddScore(175);
+  p.AddScore(100);
+  
+  player::show_player(p);
+
+  p[3] = 0;
+  player::show_player(p);
+}
+
+} // namespace player_test
+
+int main() {
+  player_test::test_player();
+}
